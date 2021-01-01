bff8e67a14954772973bda7a33c946d04af03dfd
Shaina C <scailles23@gmail.com>
2021-01-01 00:44:07 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,13 +15,11 @@ class String
   end
 
   def count_sentences
-<<<<<<< HEAD
-   array = self.split(/[\.!?] /)
-   array.count
-=======
-    self.split(".","!","?")
 
->>>>>>> 3822d8b06a16d61e6518eb00dd3668d18463fb73
+   array = self.split(/[\.!?] /)
+   array.count=self.split(".","!","?")
+
+
   end
   
 end
