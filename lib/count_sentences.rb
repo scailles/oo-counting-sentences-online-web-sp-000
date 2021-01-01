652b509acae77ad108b2e9f6d36a9f36eb6e0eb5
Shaina C <scailles23@gmail.com>
2021-01-01 00:59:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,17 +14,10 @@ class String
   self.end_with?("!")
   end
 
-  def count_sentences
-
-   array = self.split
-   count_array=[]
-   array.each do |x|
-     if x==(/[\.!?] /)
-       count_array << x
-     end
-     count_array.length
-   end
- 
+ def count_sentences
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
+  end
   
 end
 end
